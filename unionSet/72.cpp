b512da9f124051e04bafc8a9ae14cdd5f3d8223b
FuHongbao <FuHongbao@FuHongbao.com>
2019-10-02 18:55:14 +0800

    并查集结构练习

---
+++ unionSet/72.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: 72.cpp
+	> Author: victoria 
+	> Mail: 1105847344@qq.com
+	> Created Time: 2019年09月20日 星期五 13时36分57秒
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int )
+    
+    
+    return 0;
+}
+
